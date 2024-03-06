15b9cd9f3ed7ce060d0bf1a2571d01b326f34b56
oussamabootv2 <161514708+oussamabootv2@users.noreply.github.com>
2024-03-06 20:55:20 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -145,7 +145,7 @@ renderLargerThumbnail: true
 }}}, { quoted: m})
 
     /*conn.sendFile(m.chat, 'menu.png', text.trim(), m, null, )
-    /*conn.sendButton(m.chat, text.trim(), '▢ DyLux  ┃ ᴮᴼᵀ\n▢ Sígueme en Instagram\nhttps://www.instagram.com/fg98_ff', pp, [
+    /*conn.sendButton(m.chat, text.trim(), '▢ DyLux  ┃ ᴮᴼᵀ\n▢ Sígueme en Instagram\nhttps://www.instagram.com/oussama15o3', pp, [
       ['ꨄ︎ Apoyar', `${_p}donate`],
       ['⏍ Info', `${_p}botinfo`],
       ['⌬ Grupos', `${_p}gpdylux`]
