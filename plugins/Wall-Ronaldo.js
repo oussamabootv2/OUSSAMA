12f4b1108d54ae674b60bfc042d2d885fd6a8283
oussamabootv2 <161514708+oussamabootv2@users.noreply.github.com>
2024-03-10 23:43:29 +0000

    Update Wall-Ronaldo.js

--- plugins/Wall-Ronaldo.js
+++ plugins/Wall-Ronaldo.js
@@ -4,7 +4,7 @@ let res = (await axios.get(`https://raw.githubusercontent.com/Guru322/api/Guru/B
 let url = await res[Math.floor(res.length * Math.random())]
 conn.sendFile(m.chat, url, 'error.jpg', `*the best* 🥶☠️ \n *messi >>>>>> all player* 🫡 \n *Barcelona >>>> real madrid* 🥸 \n *عندك شي رأي أخر خليه لينا ف كرك سالينا* `, m)} 
 //conn.sendButton(m.chat, "*the best - الأفضل*", author, url, [['⚽ NEXT ⚽', `${usedPrefix + command}`]], m)}
-handler.help = ['cristianoronaldo', 'cr7']
+handler.help = ['cristianoronaldo', 'cr7', 'm', 'the-best', 'goat', 'الأفضل', 'ml10', 'alien']
 handler.tags = ['img']
 handler.command = /^(ronaldo|cr7)$/i
 export default handler
