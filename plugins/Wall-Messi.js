976ca6266f03bc6092256cb298137db1a5c35a0d
oussamabootv2 <161514708+oussamabootv2@users.noreply.github.com>
2024-03-10 15:44:14 +0000

    Update Wall-Messi.js

--- plugins/Wall-Messi.js
+++ plugins/Wall-Messi.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command }) => {
 let res = (await axios.get(`https://raw.githubusercontent.com/Guru322/api/Guru/BOT-JSON/Messi.json`)).data  
 let url = await res[Math.floor(res.length * Math.random())]
-conn.sendFile(m.chat, url, 'error.jpg', `*the best \n messi <<<< all player*`, m)} 
+conn.sendFile(m.chat, url, 'error.jpg', `*the best* 🥶☠️ \n *messi <<<< all player* 🫡`, m)} 
 //conn.sendButton(m.chat, "*the best - الأفضل*", author, url, [['⚽ NEXT ⚽', `${usedPrefix + command}`]], m)}
 handler.help = ['messi']
 handler.tags = ['img']
