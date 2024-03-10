7a9666eeb000f12058e68e48cf76e13d3cb46d1b
oussamabootv2 <161514708+oussamabootv2@users.noreply.github.com>
2024-03-10 23:33:05 +0000

    Update Wall-Ronaldo.js

--- plugins/Wall-Ronaldo.js
+++ plugins/Wall-Ronaldo.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command }) => {
-let cristiano = (await axios.get(`https://raw.githubusercontent.com/Guru322/api/Guru/BOT-JSON/CristianoRonaldo.json`)).data  
-let ronaldo = await cristiano[Math.floor(cristiano.length * Math.random())]
-conn.sendFile(m.chat, ronaldo, 'error.jpg', `*messi >>> ronaldo*`, m)}
-//conn.sendButton(m.chat, "*Siiiuuuuuu*", author, ronaldo, [['⚽ NEXT ⚽', `${usedPrefix + command}`]], m)}
+let res = (await axios.get(`https://raw.githubusercontent.com/Guru322/api/Guru/BOT-JSON/Messi.json`)).data  
+let url = await res[Math.floor(res.length * Math.random())]
+conn.sendFile(m.chat, url, 'error.jpg', `*the best* 🥶☠️ \n *messi >>>>>> all player* 🫡 \n *Barcelona >>>> real madrid* 🥸 \n *عندك شي رأي أخر خليه لينا ف كرك سالينا* `, m)} 
+//conn.sendButton(m.chat, "*the best - الأفضل*", author, url, [['⚽ NEXT ⚽', `${usedPrefix + command}`]], m)}
 handler.help = ['cristianoronaldo', 'cr7']
 handler.tags = ['img']
 handler.command = /^(ronaldo|cr7)$/i
